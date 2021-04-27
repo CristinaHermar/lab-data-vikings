fd5ffb5eb65bbb58b78d738490aa1e44b2ebc4c1
CristinaHermar <61674509+CristinaHermar@users.noreply.github.com>
2021-04-27 07:40:08 -0500

    Avance

--- lab-data-vikings/vikingsClases.py
+++ lab-data-vikings/vikingsClases.py
@@ -1,24 +1,62 @@
 
 # Soldier
-
-
 class Soldier:
-    pass
+    def __init__(self,health, strength):
+        self.health = health
+        self.strength = strength 
+    
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        self.health -= damage
+        
 
 # Viking
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        self.name = name
+        self.health = health
+        self.strength = strength
 
-
-class Viking:
-    pass
+    def receiveDamage(self,damage):
+        self.health -= damage
+        if self.health > 0 :
+            return ('{name} has received {damage} points of damage')
+        else:
+            return ('{name} has died in act of combat')
+    
+    def battleCry(self):
+        return ('Odin Owns you All!')
 
 # Saxon
-
-
 class Saxon:
-    pass
+    class Saxon(Soldier):
+    def __init__ (self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    def receiveDamage(self,damage):
+        self.health -= damage
+        if self.health > 0:
+            return ('A Saxon has received {damage} points of damage')
+        else:
+            return ('A Saxon has died in combat')
 
 # War
-
-
 class War:
-    pass
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+    
+    def addViking(self):
+        self.vikingArmy.append(Viking)
+    
+    def addSaxon(self):
+        self.saxonArmy.append(sak)
+        
+    def vikingAttack(self):
+        saxon = random.choice(self.saxonArmy)
+        viking = random.choice(self.vikingArmy)
+    
+     #falta
