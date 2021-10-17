fda97e0a9f60dec23375e8dc349542628a228008
CristinaHermar <cristina.hermar@gmail.com>
2021-10-16 21:31:56 -0500

    revisado

--- lab-data-vikings/vikingsClases.py
+++ lab-data-vikings/vikingsClases.py
@@ -1,62 +1,81 @@
 
 # Soldier
 class Soldier:
-    def __init__(self,health, strength):
-        self.health = health
-        self.strength = strength 
+    def __init__(self, health, strength):
+        self.health= health
+        self.strength = strength
     
-    def attack(self):
+    def attack (self):
         return self.strength
     
-    def receiveDamage(self, damage):
-        self.health -= damage
+    def receiveDamage(self,damage):
+        self.health-= damage
         
-
 # Viking
 class Viking(Soldier):
-    def __init__(self, name, health, strength):
+    def __init__ (self,name,health,strength):
         self.name = name
         self.health = health
         self.strength = strength
-
-    def receiveDamage(self,damage):
-        self.health -= damage
-        if self.health > 0 :
-            return ('{name} has received {damage} points of damage')
-        else:
-            return ('{name} has died in act of combat')
     
-    def battleCry(self):
-        return ('Odin Owns you All!')
+    def receiveDamage(damage):
+        self.health -= damage
+        if self.health > 0: 
+            return f'{self.name} has received {damage} points of damate'
+        else:
+            return f'{self.name} has died in act of combat.'
 
-# Saxon
+    #Saxon
 class Saxon:
-    class Saxon(Soldier):
     def __init__ (self, health, strength):
         self.health = health
         self.strength = strength
     
-    def receiveDamage(self,damage):
+    def receiveDamage(damage):
         self.health -= damage
-        if self.health > 0:
-            return ('A Saxon has received {damage} points of damage')
+        if self.health > 0: 
+            return f'A Saxon has received {damage} points of damate'
         else:
-            return ('A Saxon has died in combat')
+            return f'A Saxon has died in act of combat.'
 
 # War
+
+
 class War:
     def __init__(self):
         self.vikingArmy = []
         self.saxonArmy = []
-    
-    def addViking(self):
+
+    def addViking(self, Viking):
         self.vikingArmy.append(Viking)
-    
-    def addSaxon(self):
-        self.saxonArmy.append(sak)
-        
+
+    def addSaxon(self, Saxon):
+        self.saxonArmy.append(Saxon)
+
     def vikingAttack(self):
-        saxon = random.choice(self.saxonArmy)
-        viking = random.choice(self.vikingArmy)
-    
-     #falta
+        rand_viking = random.randint(0, len(self.vikingArmy) - 1)
+        rand_saxon = random.randint(0, len(self.saxonArmy) - 1)
+        selected_viking = self.vikingArmy[rand_viking]
+        selected_saxon = self.saxonArmy[rand_saxon]
+        result = selected_saxon.receiveDamage(selected_viking.strength)
+        if selected_saxon.health <= 0:
+            self.saxonArmy.pop(rand_saxon)
+        return result
+
+    def saxonAttack(self):
+        rand_viking = random.randint(0, len(self.vikingArmy) - 1)
+        rand_saxon = random.randint(0, len(self.saxonArmy) - 1)
+        selected_viking = self.vikingArmy[rand_viking]
+        selected_saxon = self.saxonArmy[rand_saxon]
+        result = selected_viking.receiveDamage(selected_saxon.strength)
+        if selected_viking.health <= 0:
+            self.vikingArmy.pop(rand_viking)
+        return result
+
+    def showStatus(self):
+        if len(self.vikingArmy) == 0:
+            return "Saxons have fought for their lives and survive another day..."
+        elif len(self.saxonArmy) == 0:
+            return "Vikings have won the war of the century!"
+        else:
+            return "Vikings and Saxons are still in the thick of battle."
